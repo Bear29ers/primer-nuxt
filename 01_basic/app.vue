16ce7801d4d02277ab449ab709f884746c6d452f
Yuta Okuma <y.dream.nest@icloud.com>
2023-05-14 08:55:20 +0900

    feat: :sparkles: カスタムレイアウトファイル

--- 01_basic/app.vue
+++ 01_basic/app.vue
@@ -1,5 +1,15 @@
+<script lang="ts" setup>
+const admin = false;
+const layout = admin ? 'custom' : 'default';
+</script>
+
 <template>
-  <div>
+  <!-- NuxtLayoutタグで自動でlayoutsディレクトリに存在するdefault.vueの内容が適用される -->
+  <!-- v-bindのname propsを利用して動的にレイアウトを変更する -->
+  <NuxtLayout :name="layout">
+    <!-- 全体にデフォルトで設定するレイアウトファイルを設定する場合 -->
+    <!-- <NuxtLayout name="custom"> -->
+    <!-- pages以下の内容を適用するためにNuxtPageタグを追加する -->
     <NuxtPage />
-  </div>
+  </NuxtLayout>
 </template>
--- 01_basic/pages/about.vue
+++ 01_basic/pages/about.vue
@@ -1,4 +1,9 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+// 特定のページでレイアウトを適用したくない場合は、definePageMeta関数で制御する
+definePageMeta({
+  layout: 'custom',
+});
+</script>
 
 <template>
   <h1>About Page</h1>
