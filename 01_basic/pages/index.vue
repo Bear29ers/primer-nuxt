f6eab0342b75628e9581c5903c2c9bdb89bf655e
Yuta Okuma <y.dream.nest@icloud.com>
2023-05-18 21:27:34 +0900

    feat: :sparkles: 名前付きslotの設定

--- 01_basic/pages/index.vue
+++ 01_basic/pages/index.vue
@@ -1,10 +1,11 @@
 <script lang="ts" setup>
 // useRoute関数を利用してroute.meta.layoutの値を設定することも動的に可能
+/*
 const route = useRoute();
 const enableCustomLayout = () => {
   route.meta.layout = 'custom';
 };
-
+*/
 definePageMeta({
   layout: false,
 });
@@ -12,7 +13,12 @@ definePageMeta({
 
 <template>
   <div>
-    <button @click="enableCustomLayout">Update Layout</button>
+    <!-- NuxtLayoutに明示的にnameの設定が必須になる。またNuxtLayoutがないとエラーになる（名前付きSlot） -->
+    <NuxtLayout name="default">
+      <template #header>ヘッダー</template>
+      <h1>Main Page</h1>
+    </NuxtLayout>
+    <!-- <button @click="enableCustomLayout">Update Layout</button> -->
     <!-- <NuxtLayout name="custom"> -->
     <!--   <h1>Main Page</h1> -->
     <!-- </NuxtLayout> -->
