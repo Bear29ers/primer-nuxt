bc6dfbcea6885eb687e838bc1d71e87f16b95df7
Yuta Okuma <y.dream.nest@icloud.com>
2023-05-19 20:20:07 +0900

    feat: :sparkles: publicディレクトリの設定

--- 01_basic/nuxt.config.ts
+++ 01_basic/nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ['/assets/css/style.css'],
+  app: {
+    head: {
+      title: 'Nuxt 3 Basic',
+      meta: [{ name: 'description', content: 'Nuxt 3 for Beginners' }],
+      link: [{ rel: 'icon', href: '/favicon.ico' }],
+    },
+  },
 });
