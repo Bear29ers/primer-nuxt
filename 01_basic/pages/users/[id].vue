93cbbf03bce3bb2ebb4a6dcfff0d297fde79d0f2
Yuta Okuma <y.dream.nest@icloud.com>
2023-05-19 21:23:42 +0900

    feat: :sparkles: Dynamicルーティング

---
+++ 01_basic/pages/users/[id].vue
@@ -0,0 +1,13 @@
+<!-- ダイナミックルーティングを利用してパラメータごとに異なるページを表示する -->
+<script lang="ts" setup>
+import { useRoute } from 'vue-router';
+
+const router = useRoute();
+</script>
+
+<template>
+  <div>
+    <p>ユーザーID: {{ $route.params.id }}</p>
+    <p>ユーザーID: {{ router.params.id }}</p>
+  </div>
+</template>
