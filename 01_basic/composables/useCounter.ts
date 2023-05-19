7b5ce43b1f411559af801623266e95b43ecd8a6e
Yuta Okuma <y.dream.nest@icloud.com>
2023-05-19 20:31:44 +0900

    feat: :sparkles: composablesディレクトリ

---
+++ 01_basic/composables/useCounter.ts
@@ -0,0 +1,13 @@
+import { ref } from 'vue';
+
+export const useCounter = (initialValue: number) => {
+  const count = ref(initialValue);
+  const inc = () => (count.value = count.value + 1);
+  const dec = () => (count.value = count.value - 1);
+
+  return {
+    count,
+    inc,
+    dec,
+  };
+};
---
+++ 01_basic/pages/counter.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { useCounter } from '../composables/useCounter';
+
+const { count, inc, dec } = useCounter(100);
+</script>
+
+<template>
+  <div>
+    <h1>Main Page</h1>
+    <div>Count: {{ count }}</div>
+    <div>
+      <button @click="() => inc()">Increase</button>
+      <button @click="() => dec()">Decrease</button>
+    </div>
+  </div>
+</template>
