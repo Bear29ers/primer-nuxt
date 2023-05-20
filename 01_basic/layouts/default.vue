c4adec169c001ba746ec0b9ae9ce0327e273c44c
Yuta Okuma <y.dream.nest@icloud.com>
2023-05-20 09:22:40 +0900

    feat: :sparkles: Page Transitions

--- 01_basic/layouts/default.vue
+++ 01_basic/layouts/default.vue
@@ -1,3 +1,10 @@
+<script lang="ts" setup>
+definePageMeta({
+  // ページ単位でPage Transitionを設定する
+  pageTransition: false,
+});
+</script>
+
 <template>
   <div>
     <!-- ディレクトリ構造が反映される -->
@@ -8,3 +15,16 @@
     <slot />
   </div>
 </template>
+
+<style>
+.page-entry-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
--- 01_basic/nuxt.config.ts
+++ 01_basic/nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ['/assets/css/style.css'],
+  // css: ['/assets/css/style.css'],
+
   app: {
+    pageTransition: { name: 'page', mode: 'out-in' },
     head: {
       title: 'Nuxt 3 Basic',
       meta: [{ name: 'description', content: 'Nuxt 3 for Beginners' }],
