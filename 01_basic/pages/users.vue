f8e7c88d1e1245018a5b109c441bd776fcdac9c7
Yuta Okuma <y.dream.nest@icloud.com>
2023-05-25 17:00:41 +0900

    feat: :sparkles: NuxtErrorBoundary

--- 01_basic/pages/users.vue
+++ 01_basic/pages/users.vue
@@ -1,7 +1,25 @@
+<script lang="ts" setup>
+const resetError = (error) => {
+  error.value = null;
+};
+// エラー情報を取得して別の処理を行うためにエラーイベントを設定する
+const someErrorLogger = (error) => {
+  // 取得したエラーで別の処理を行う
+  console.log('someErrorLogger', error);
+};
+</script>
+
 <template>
   <div>
     <h1>Usersページ</h1>
     <!-- NuxtPageタグでページのネスト化を行う -->
-    <NuxtPage />
+    <!-- NuxtErrorBoundaryでエラーをキャッチして表示内容を制御する -->
+    <NuxtErrorBoundary @error="someErrorLogger">
+      <NuxtPage />
+      <template #error="{ error }">
+        <p>{{ error }}</p>
+        <button @click="resetError(error)">Clear Error</button>
+      </template>
+    </NuxtErrorBoundary>
   </div>
 </template>
--- 01_basic/pages/users/list.vue
+++ 01_basic/pages/users/list.vue
@@ -1,12 +1,20 @@
 <script lang="ts" setup>
-import { useCounter } from '~/composables/useCounter';
-const counter = useCounter();
+// import { useCounter } from '~/composables/useCounter';
+// const counter = useCounter();
+const counter = ref(0);
+const inc = () => {
+  // createErrorはNuxtから提供されている関数で、クライアント側でもサーバー側でも利用でき、エラーオブジェクトを作成できる
+  throw createError('エラー発生');
+  counter.value++;
+};
 </script>
 
 <template>
   <div>
     <h1>User Listページ</h1>
+    <h2>Counter</h2>
     <p>Count: {{ counter }}</p>
+    <div><button @click="inc">+</button></div>
     <NuxtPage />
   </div>
 </template>
