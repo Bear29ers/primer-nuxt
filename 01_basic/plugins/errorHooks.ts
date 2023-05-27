d25abcea34aef42045e16272a67d90f54231fee0
Yuta Okuma <y.dream.nest@icloud.com>
2023-05-28 07:52:30 +0900

    feat: :sparkles: hooksによるエラーの取得

---
+++ 01_basic/plugins/errorHooks.ts
@@ -0,0 +1,9 @@
+import { defineNuxtPlugin } from 'nuxt/app';
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.hook('vue:error', (err, target, info) => {
+    console.log('err', err);
+    console.log('target', target);
+    console.log('info', info);
+  });
+});
